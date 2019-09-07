cc10112129834bb4dd06515f9ae984861890fffa
juchitt <juchis2@gmail.com>
2019-09-07 16:38:57 +0900

    Tutorial 1번

---
+++ rabbitmq_practice/src/main/java/tut1/Tut1Sender.java
@@ -0,0 +1,24 @@
+package tut1;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class Tut1Sender {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private Queue queue;
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void send() {
+        String msg = "Hello world!!";
+
+        rabbitTemplate.convertAndSend(queue.getName(),msg);
+
+        System.out.println(" [x] Sent '"+ msg + "'");
+    }
+}
